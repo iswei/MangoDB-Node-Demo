4954c5695bad02948355745fd3f92ec3aaf07f49
Wei Lin <iswei@live.com>
2015-03-05 03:44:54 -0500

    create mongo method and virtuals (custom property) and call the method

--- db/script.js
+++ db/script.js
@@ -15,10 +15,46 @@ db.once('open', function (callback) {
     // yay, db open and ready!
 
     var kittySchema = mongoose.Schema({
-        name: String
+        name: {
+            first: String,
+            last: String
+        },
+        age: Number
+    });
+//virtual is not save to database, but the key will exist in the model
+    kittySchema.virtual('name.full').get(function(){
+        "use strict";
+       return this.name.first + " " + this.name.last;
+    });
+//method needs to be added to the schema before compiling with model()
+    kittySchema.methods.speak = function () {
+        var greeting = "Meow name is " + this.name.full;
+        greeting += " and I am " + this.age + " years old.";
+        console.log(greeting);
+    }
+
+    var Kitten = mongoose.model('Kitten', kittySchema)
+
+    Kitten.findOne({"age":3}, function(err, koda){
+        "use strict";
+        if(err)return console.error(err);
+        console.log(koda.name.full);
+        koda.speak();
     });
 
-    var Kitten = mongoose.model('Kitten', kittySchema);
+
+/*
+    var koda = new Kitten({
+        name: {
+            first: 'Koda',
+            last: 'Bug'
+        },
+        age: 3
+    });
+
+    koda.save();
+
+
     Kitten.findOne({name: 'Koda'}, function(err,koda){
         "use strict";
         if(err) return console.error(err);
@@ -31,6 +67,10 @@ db.once('open', function (callback) {
             console.log("Koda document saved success");
         });
 
+
+
     });
+
+ */
 });
 
